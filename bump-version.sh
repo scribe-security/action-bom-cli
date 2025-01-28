f07f58df7c95eee48944d882164c3185b8106030
houdini91 <mdstrauss91@gmail.com>
2025-01-28 11:53:56 +0200

    back

--- bump-version.sh
+++ bump-version.sh
@@ -3,6 +3,8 @@
 set -ex
 
 version=$1
+git checkout dev
+git pull origin dev
 git branch -D  prep_release/$version || true
 git checkout -b prep_release/$version || git checkout prep_release/$version || true
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"

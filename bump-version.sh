37aad62e8891bd1f63c8f8d51a56268068f19e21
houdini91 <mdstrauss91@gmail.com>
2025-01-28 12:00:01 +0200

    back

--- bump-version.sh
+++ bump-version.sh
@@ -7,21 +7,9 @@ git checkout dev
 git pull origin dev
 git branch -D  prep_release/$version || true
 git checkout -b prep_release/$version || git checkout prep_release/$version || true
-echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-cat action.yml
-
 sed -i "s/\(sh -s -- -t valint\) -D$/\1:$version/" action.yml
-
-echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-cat action.yml
-
-
 git add action.yml || true
 git commit -m "bump version" || true
 git fetch origin master
-
-#echo "##########################"
-#cat action.yml
-
 git merge --no-edit -s ours origin/master || true
 git push -f origin prep_release/$version || true
